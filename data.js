5a3e565e85a26f0eab0ee1a05b9f3d277420ced9
Fahim Ahamed <121462098+fahimahamed1@users.noreply.github.com>
2025-09-07 01:08:56 +0600

    Update impostorProfileUrl in data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ module.exports = {
   victimEmail: "your.email@example.com",
   impostorName: "Impostor Full Name",
   impostorEmail: "impostor.email@example.com",
-  impostorProfileUrl: "https://www.facebook.com/rifat.khan.77128",
+  impostorProfileUrl: "https://www.facebook.com/username",
   message: "This account is impersonating me. Please take action.",
   idFiles: ["your_id.jpg"] // list of IDs to upload (relative to main script)
 };
